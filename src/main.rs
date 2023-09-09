a259be218d6a2ef272aa17d95f7adff4e86fc184
张浩杰 <havoc_zhang@yeah.net>
2023-09-09 21:40:21 +0800

    feat: 增加tracing记录日志

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+use time::macros::format_description;
+use tracing_subscriber::{
+    fmt::{self, time::LocalTime},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+
+#[tokio::main]
+async fn main() {
+    let timer = LocalTime::new(format_description!(
+        "[year]-[month]-[day] [hour]:[minute]:[second]"
+    ));
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_timer(timer))
+        .init();
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
